22969aa6df37100519aad100e339c10f65c8f551
kamiri61 <kamiri6161@gmail.com>
2020-05-17 13:46:39 +0900

    DL方法変更

--- exan.py
+++ exan.py
@@ -1,59 +1,65 @@
-# -*- coding: utf-8 -*-
+from __future__ import print_function
+import keras
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 
-import time
+batch_size = 128
+num_classes = 49
+epochs = 50
 
-import cv2
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+# load dataset
+x_train = np.load('dataset/k49-train-imgs.npz')['arr_0']
+x_test = np.load('dataset/k49-test-imgs.npz')['arr_0']
+y_train = np.load('dataset/k49-train-labels.npz')['arr_0']
+y_test = np.load('dataset/k49-test-labels.npz')['arr_0']
 
-# フレームサイズ
-FRAME_W = 320
-FRAME_H = 240
+# change data shape
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
 
-# 顔検出用カスケー分類器
-cascadeFilePath = '/usr/local/share/OpenCV/lbpcascades/lbpcascade_frontalface.xml'
-# 分類器をセットします
-frontalFaceCascadeClf = cv2.CascadeClassifier(cascadeFilePath)
+# change data value
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
-# カメラインスタンス作成
-v_camera = PiCamera()
-# カメラの解像度を設定します
-v_camera.resolution = (FRAME_W, FRAME_H)
-# カメラのフレームレートを設定します(FPS)
-v_camera.framerate = 16
-# v_cameraインスタンスから、画像のRGB配列取得します。そのままnumpyで処理できます
-rawCapture = PiRGBArray(v_camera, size=(FRAME_W, FRAME_H))
-# timeモジュールのsleep関数です。ここでは1秒待ちます
-time.sleep(1)
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
-# v_camera.capture_continuousは明示的に停止指示があるまで、無限にイメージを取得し続けます
-for raw_camera_data in v_camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # 画像を配列データを変数frameに代入します
-    frame = raw_camera_data.array
-    # 取得したBGRデータをグレースケールに変換します。
-    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # グレースケールイメージに変換します
-    gray_image = cv2.equalizeHist(gray_image)
-    # 顔検出
-    multipleFaces = frontalFaceCascadeClf.detectMultiScale(gray_image, 1.1, 3, 0, (20, 20))
-    # 線の色
-    line_color = (255, 102, 51)
-    # 文字の色
-    font_color = (255, 102, 51)
-    # 検出した顔に枠を書く
-    for (x, y, width, height) in multipleFaces:
-        # 見つかった顔を線で囲みます
-        cv2.rectangle(frame, (x, y), (x + width, y + height), line_color, 2)
-        # 顔の上に「FACE」という文字を表示します
-        cv2.putText(frame, 'FACE', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, font_color, 1, cv2.LINE_AA)
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.summary()
 
-    # ビデオに表示 
-    cv2.imshow('Video', frame)
-    # キーボードの入力を処理します
-    key = cv2.waitKey(1) & 0xFF
-    rawCapture.truncate(0)
-    # [q]キーと入力すると、プログラムを終了させます
-    if key == ord("q"):
-        break
-# 終了処理、プログラムが作ったウィンドウを全部閉じます
-cv2.destroyAllWindows()
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# save model and weights
+open('k_mnist_cnn_model.json','w').write(model.to_json())
+model.save_weights('k_mnist_cnn_weights.h5')

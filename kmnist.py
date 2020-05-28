a0ca60ff2fb616d2b55ede1d9d19d2f39d058873
kamiri61 <kamiri6161@gmail.com>
2020-05-28 21:17:26 +0900

    　

--- kmnist.py
+++ kmnist.py
@@ -12,10 +12,7 @@ import re
 label_dict = {"0":"あ", "1":"い", "2":"う", "3":"え", "4":"お", "5":"か", "6":"き", "7":"く", "8":"け", "9":"こ", "10":"さ", "11":"し", "12":"す", "13":"せ", "14":"そ", "15":"た", "16":"ち", "17":"つ", "18":"て", "19":"と", "20":"な", "21":"に", "22":"ぬ", "23":"ね", "24":"の", "25":"は", "26":"ひ", "27":"ふ", "28":"へ", "29":"ほ", "30":"ま", "31":"み", "32":"む", "33":"め", "34":"も", "35":"や", "36":"ゆ", "37":"よ", "38":"ら", "39":"り", "40":"る", "41":"れ", "42":"ろ", "43":"わ", "44":"ゐ", "45":"ゑ", "46":"を", "47":"ん", "48":"ゝ", "49":"-"}
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-#model = load_model("model/keras-kmnist-model.h5")
 model_path="model/keras-kmnist-model.h5"
-#model_arc_str = open(model_path, encoding="latin1").read()
-#model = load_model(model_arc_str)
 model =model_from_json(open("model/k_mnist_cnn_model.json").read())
 model.load_weights("model/k_mnist_cnn_weights.h5")
 

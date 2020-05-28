5d64c320bda1446d3d06e109ec5b3cfb6846e896
kamiri61 <kamiri6161@gmail.com>
2020-05-28 21:16:22 +0900

    日本語の表示

--- kmnist.py
+++ kmnist.py
@@ -2,13 +2,14 @@
 import cv2
 import keras
 import numpy as np
-from PIL import Image
+import collections
+from PIL import ImageFont, ImageDraw, Image
 from keras.models import model_from_json#load_model
 from keras.preprocessing.image import array_to_img, img_to_array,load_img
 import os
 import re
 
-label_dict = {"0":"あ", "1":"い", "2":"う", "3":"え", "4":"お", "5":"か", "6":"き", "7":"く", "8":"け", "9":"こ", "10":"さ", "11":"し", "12":"す", "13":"せ", "14":"そ", "15":"た", "16":"ち", "17":"つ", "18":"て", "19":"と", "20":"な", "21":"に", "22":"ぬ", "23":"ね", "24":"の", "25":"は", "26":"ひ", "27":"ふ", "28":"へ", "29":"ほ", "30":"ま", "31":"み", "32":"む", "33":"め", "34":"も", "35":"や", "36":"ゆ", "37":"よ", "38":"ら", "39":"り", "40":"る", "41":"れ", "42":"ろ", "43":"わ", "44":"ゐ", "45":"ゑ", "46":"を", "47":"ん", "48":"ゝ"}
+label_dict = {"0":"あ", "1":"い", "2":"う", "3":"え", "4":"お", "5":"か", "6":"き", "7":"く", "8":"け", "9":"こ", "10":"さ", "11":"し", "12":"す", "13":"せ", "14":"そ", "15":"た", "16":"ち", "17":"つ", "18":"て", "19":"と", "20":"な", "21":"に", "22":"ぬ", "23":"ね", "24":"の", "25":"は", "26":"ひ", "27":"ふ", "28":"へ", "29":"ほ", "30":"ま", "31":"み", "32":"む", "33":"め", "34":"も", "35":"や", "36":"ゆ", "37":"よ", "38":"ら", "39":"り", "40":"る", "41":"れ", "42":"ろ", "43":"わ", "44":"ゐ", "45":"ゑ", "46":"を", "47":"ん", "48":"ゝ", "49":"-"}
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 #model = load_model("model/keras-kmnist-model.h5")
@@ -36,17 +37,35 @@ def check_number(frame):
     ans=str(num.argmax())
     return ans
 
+def img_add_msg(img, message):
+    font_path = 'C:\Windows\Fonts\meiryo.ttc'           # Windowsのフォントファイルへのパス
+    font_size = 24                                      # フォントサイズ
+    font = ImageFont.truetype(font_path, font_size)     # PILでフォントを定義
+    img = Image.fromarray(img)                          # cv2(NumPy)型の画像をPIL型に変換
+    draw = ImageDraw.Draw(img)                          # 描画用のDraw関数を用意
+    # テキストを描画（位置、文章、フォント、文字色（BGR+α）を指定）
+    draw.text((20, 50), message, font=font, fill=(255, 0, 0, 0))
+    img = np.array(img)                                 # PIL型の画像をcv2(NumPy)型に変換
+    return img 
+
 def main():
     cap = cv2.VideoCapture(0)
+    ans = []
+    text = "49"
     while(cap.isOpened()):
         ret,frame=cap.read()
         frame1=frame
         frame = cv2.cvtColor(cap.read()[1], cv2.COLOR_RGB2GRAY)
         img=remake(frame)
         ansnum=check_number(img)
-        tex=label_dict[ansnum]
+        ans.append(ansnum)
+        if (len(ans)>10):
+            c = collections.Counter(ans)
+            text = c.most_common()[0][0]
+            ans.clear()
+        tex=label_dict[text]
 
-        cv2.putText(cv2.rectangle(frame1, (200, 150), (400, 350), (255, 0, 0), 1, 4), tex, (20,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,200), 2, cv2.LINE_AA)
+        frame1 = img_add_msg(frame1, tex)
         cv2.imshow("num", frame1)
 
         key = cv2.waitKey(10)
